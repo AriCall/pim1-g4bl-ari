47eef212f6210d4a54e1764a26a15d3bc57c017f
AriCall <aricall@uw.edu>
2026-05-21 13:08:44 -0700

    Add files via upload

---
+++ magnet_tuning_op.py
@@ -0,0 +1,31 @@
+import numpy as np
+import uproot
+from scipy import optimize
+import subprocess
+import os
+
+#Reminder; you need to clear the output of any previous run.
+
+#define a function to be opimized; returns the sqare root of the squared stds
+def beam_deviance(scale15, scale16,scale17,scale18):
+    os.environ["SCALE15"] = scale15
+    os.environ["SCALE16"] = scale16
+    os.environ["SCALE17"] = scale17
+    os.environ["SCALE18"] = scale18
+    subprocess.run("g4bl $PIM1/piM1_mu.g4bl", shell=True, check=True)
+    with uproot.open("piM1_plastic_decay_bend_air_mu+_155_1.0_300_12348.root") as file:
+        #get the standard deviation in x and y at the end
+        branch_name = 'NTuple/22150'
+        x_vals = file[branch_name]['x'].array(library="np")
+        y_vals = file[branch_name]['y'].array(library="np")
+        x_std = np.std(x_vals)
+        y_std = np.std(y_vals)
+    os.remove("piM1_plastic_decay_bend_air_mu+_155_1.0_300_12348.root")
+    return np.sqrt(x_std**2+y_std**2)
+
+#Arbitrary bounds; if the minimum is found to be very close to 4 or 0.001 for any of them, I will change them.
+bounds = [(0.001,4),(0.001,4),(0.001,4),(0.001,4)]
+
+results = dict()
+results['DE'] = optimize.differential_evolution(beam_deviance,bounds) #Add maxiter = n if this takes too long
+print(results['DE'])
