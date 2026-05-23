e2969babaad8b32afb24eab006881a22bf35ef28
AriCall <aricall@uw.edu>
2026-05-23 14:37:09 -0700

    Update magnet_tuning_op.py

--- magnet_tuning_op.py
+++ magnet_tuning_op.py
@@ -28,5 +28,5 @@ bounds = [(0.001,4),(0.001,4),(0.001,4),(0.001,4)]
 results = dict()
 results['DE'] = optimize.differential_evolution(beam_deviance,bounds,workers=1,maxiter=500) #Add maxiter = n if this takes too long
 print(results['DE'])
-with open("tuningOutput.txt", "w") as text_file:
-    text_file.write(f'{results['DE'][x]}, {results['DE'][fun]})
+#with open("tuningOutput.txt", "w") as text_file:
+#    text_file.write(f'{results['DE'][x]}, {results['DE'][fun]})
